beed92b35f75e3e4174dbeb34e309880d9f700ab
kangyu <kyuang@126.com>
2016-06-15 20:27:07 +0800

    123

--- Application/Home/Controller/AdminController.class.php
+++ Application/Home/Controller/AdminController.class.php
@@ -21,9 +21,34 @@ class AdminController extends Controller {
     //发布话题
     public function addhua()
     {
-      $url = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=ACCESS_TOKEN&code=CODE";
+      $corpid = 'wxdfb65973db0deb4d';
+      $secrect = 'HXOk8frb4AaAjXvNKXNxfMegKvwQCrTVZmmeJt2HIk4mjkDtNILOMLRCF9Fw5OVV';
+      // 调用接口凭证 
+      $access_token = getAccessToken($corpid,$secrect);
+      $code = $_GET['code'];
+      $url = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=$access_token&code=$code";
+      $res = curl($url,'');
+      $userid = json_decode($res)['UserId'];
+      $url = "https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=$access_token&userid=$userid";
+      $res = curl($url,'');
+      echo $res;die;
+      $user = json_decode($res);
+      var_dump($user);die;
+    }
 
-      $this->display('addhua');
+    function curl( $url, $sendData )
+    {
+      $ch = curl_init ();
+      curl_setopt ( $ch, CURLOPT_URL, $url );
+      curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+      curl_setopt ( $ch, CURLOPT_POST, 1 );
+      curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+      curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+      curl_setopt ( $ch, CURLOPT_POSTFIELDS, $sendData );
+      $return = curl_exec ( $ch );
+      curl_close ( $ch );
+      return $return;
     }
 
 }
