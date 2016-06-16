91cdc274cce1766df3e15eed0ffccf951fab0ffe
kangyu <kyuang@126.com>
2016-06-16 09:07:06 +0800

    ok

--- Application/Home/Controller/AdminController.class.php
+++ Application/Home/Controller/AdminController.class.php
@@ -28,22 +28,31 @@ class AdminController extends Controller {
       $corpid = 'wxdfb65973db0deb4d';
       $secrect = 'HXOk8frb4AaAjXvNKXNxfMegKvwQCrTVZmmeJt2HIk4mjkDtNILOMLRCF9Fw5OVV';
       // 调用接口凭证 
+     
       if(empty($_SESSION['access_token']))
       {
         $access_token = $this->getAccessToken($corpid,$secrect);
         $_SESSION['access_token'] = $access_token;
+      }else{
+	$access_token = $_SESSION['access_token'];
       }
-
       $code = $_GET['code'];
-      $url = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=$access_token&code=$code";
-      $res = $this->curl($url,'');
+echo $code; 
+
+$url = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=$access_token&code=$code";
+      //$res = $this->curl($url,'');
+      $res = file_get_contents($url);
+      print_r($res);
+echo "<br>";
+echo $access_token;die;
       $userid = json_decode($res)['UserId'];
       $url = "https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=$access_token&userid=$userid";
       $res = $this->curl($url,'');
-      echo $res;die;
+      echo $res;
       $user = json_decode($res);
-      var_dump($user);die;
-    }
+      //var_dump($user);die;
+      $this->display('addhua');  
+  }
 
     function curl( $url, $sendData)
     {
