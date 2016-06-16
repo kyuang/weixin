536e66b0de95738c94af462b45636521affdca02
kangyu <kyuang@126.com>
2016-06-16 15:46:45 +0800

    123

--- Application/Runtime/Cache/Home/5a6e40eedc8109aa84586b754b1ccd5a.php
+++ Application/Runtime/Cache/Home/5a6e40eedc8109aa84586b754b1ccd5a.php
@@ -33,7 +33,7 @@
 	  </div>
 	  <div class="form-group">
 	    <label for="exampleInputFile">选择参与人员</label>
-	    <a href="/weixin_tongshi/index.php/Home/Admin/tongxun"><input type="button" id="button" value="通讯录"></a>
+	    <a href="/weixin/index.php/Home/Admin/tongxun"><input type="button" id="button" value="通讯录"></a>
 	  </div>
 	  <button type="submit" class="btn btn-default">保存为待提交</button>
 	  <button type="submit" class="btn btn-default">直接提交</button>
